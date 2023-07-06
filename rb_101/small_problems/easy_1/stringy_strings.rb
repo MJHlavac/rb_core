f5017da61d20588edc8f9b1c464221a7d6fbe3ae
Marc Hlavac <mhlava15@gmail.com>
2023-07-06 20:27:05 +0000

    Easy 1 problems

--- rb_101/small_problems/easy_1/stringy_strings.rb
+++ rb_101/small_problems/easy_1/stringy_strings.rb
@@ -1,12 +1,24 @@
-def stringy(integer)
-	if integer.even?
-		'10' * (integer / 2)
-	else
-		'10' * (integer / 2) + '1'
-	end
+def stringy(integer, default = 1)
+  case default
+  when 1
+    if integer.even?
+      '10' * (integer / 2)
+    else
+      '10' * (integer / 2) + '1'
+    end
+  when 0
+    if integer.even?
+      '01' * (integer / 2)
+    else
+      '01' * (integer / 2) + '0'
+    end
+  end
 end
 
-puts stringy(6)
-puts stringy(9)
-puts stringy(4)
-puts stringy(7)
+puts stringy(6) == '101010'
+puts stringy(9) == '101010101'
+puts stringy(4) == '1010'
+puts stringy(1) == '1'
+puts stringy(7, 0) == '0101010'
+puts stringy(2, 0) == '01'
+puts stringy(1, 0) == '0'
